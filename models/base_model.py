1724ab47859e10c5535203c9ae790ffbc73cc8d3
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-09 02:27:14 +0000

    just branching and some changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" module that defines BaseModel class """
+"""
+module that defines BaseModel class
+"""
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """ BaseModel that defines all common
-    attributes/methods for other classes """
+    """
+    BaseModel that defines all common attributes/methods
+    for other classes
+    """
 
     def __init__(self):
         self.id = str(uuid4())
@@ -14,18 +18,24 @@ class BaseModel:
         self.updated_at = datetime.now().isoformat()
 
     def save(self):
-        """ update public instance updated_at with current time """
+        """
+        Update public instance updated_at with current time
+        """
         self.updated_at = datetime.now().isoformat()
 
     def to_dict(self):
-        """ returns the dictornary representation of BaseModel
-        instance with class name included """
+        """
+        Returns the dictornary representation of BaseModel
+        instance with class name included
+        """
         my_dict = dict()
         my_dict = self.__dict__
         my_dict['__class__'] = self.__class__.__name__
         return my_dict
 
     def __str__(self):
-        """ return the string representation of BaseModel instance """
+        """
+        Returns the string representation of BaseModel instance
+        """
         class_name = self.__class__.__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}"
