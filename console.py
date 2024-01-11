9d93d5c6c3b8179ad6d2a389ae74a94a7142f6fa
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-11 10:19:36 +0000

    implementation fo task 7: console v0.1

--- console.py
+++ console.py
@@ -3,6 +3,9 @@
 Command interpreter module
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
+from shlex import split
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,22 +19,146 @@ class HBNBCommand(cmd.Cmd):
     - do_quit(arg): Implements the 'quit' command to exit the program.
     - do_EOF(arg): Implements the 'EOF' command to exit the program.
     - emptyline(): Does nothing on an empty line.
+    - do_create(arg): Creates a new instance of BaseModel.
+    - do_show(arg): Prints the string representation of an instance.
+    - do_destroy(arg): Deletes an instance.
+    - do_all(arg): Prints all string representation of all instances.
+    - do_update(arg): Updates an instance based on the class name and id.
     """
 
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """
+        Quit command to exit the program
+        """
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program"""
+        """
+        EOF command to exit the program
+        """
         print()
         return True
 
     def emptyline(self):
-        """Do nothing on empty line"""
+        """
+        Do nothing on empty line
+        """
         pass
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel, saves
+        it (to the JSON file) and prints the id.
+        """
+        args = split(arg)
+        if not args or args[0] == "":
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(args[0])()
+            new_instance.save()
+            print(new_instance.id)
+        except Exception as e:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based
+        on the class name and id.
+        """
+        args = split(arg)
+        if not args or args[0] == "":
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            instance_id = args[1]
+            key = "{}.{}".format(class_name, instance_id)
+            obj = storage.all().get(key)
+            if not obj:
+                print("** no instance found **")
+                return
+            print(obj)
+        except Exception as e:
+            print("** {}". format(e))
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        (saves the change into the JSON file)
+        """
+        args = split(arg)
+        if not args or args[0] == "":
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            instance_id = args[1]
+            key = "{}.{}".format(class_name, instance_id)
+            obj = storage.all().get(key)
+            if not obj:
+                print("** no instance found **")
+                return
+            del storage.all()[key]
+            storage.save()
+        except Exception as e:
+            print("** {}".format(e))
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or
+        not on the class name
+        """
+        args = split(arg)
+        obj_list = []
+        if not args or arg[0] == "":
+            for obj in storage.all().values():
+                obj_list.append(str(obj))
+            print(obj_list)
+            return
+        try:
+            class_name = args[0]
+            for obj in storage.all().values():
+                if class_name == obj.__class__.__name__:
+                    obj_list.append(str(obj))
+            if not obj_list:
+                print("** class doesn't exit **")
+                return
+            print(obj_list)
+        except Exception as e:
+            print("** {}".format(e))
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding
+        or updating attribute (save the change into the JSON file).
+        """
+        args = split(arg)
+        if not args or args[0] == "":
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            instance_id = args[1]
+            key = "{}.{}".format(class_name, instance_id)
+            obj = storage.all().get(key)
+            if not obj:
+                print("** no instance found **")
+                return
+            if len(args) < 3:
+                print("** attribute name missing **")
+                return
+            attribute_name = args[2]
+            if len(args) < 4:
+                print("** value missing **")
+                return
+            value = args[3]
+            setattr(obj, attribute_name, value)
+            obj.save()
+        except Exception as e:
+            print("** {}".format(e))
 
 
 if __name__ == '__main__':
