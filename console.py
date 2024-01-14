fbdcb1647d376a68e333b55b84dde62734b0086a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-14 12:52:31 +0000

    implementation of task 15

--- console.py
+++ console.py
@@ -308,6 +308,19 @@ class HBNBCommand(cmd.Cmd):
                 id_arg = id_arg.strip("'")
                 arg = class_arg + ' ' + id_arg
                 HBNBCommand.do_destroy(self, arg)
+            elif command == 'update':
+                args = args[1].split(',')
+                id_arg = args[0].strip("'")
+                id_arg = id_arg.strip('"')
+                name_arg = args[1].strip(',')
+                val_arg = args[2]
+                name_arg = name_arg.strip(' ')
+                name_arg = name_arg.strip("'")
+                name_arg = name_arg.strip('"')
+                val_arg = val_arg.strip(' ')
+                val_arg = val_arg.strip(')')
+                arg = class_arg + ' ' + id_arg + ' ' + name_arg + ' ' + val_arg
+                HBNBCommand.do_update(self, arg)
             else:
                 print("*** Unknown syntax: {}".format(line))
         except IndexError:
